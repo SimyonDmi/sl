79b5544e0b0f1201cbab42edfa869ca1cb68fb1f
SimyonDmi <xartman90@mail.ru>
2018-02-22 09:09:28 +0700

    umn

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
         {
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+            Console.WriteLine("{0}*{1}={2}", a, b, a * b);
             
             Console.ReadKey();
         }
