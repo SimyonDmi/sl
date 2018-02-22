b26d73f6c7a0344a30372101669a7209da202d83
SimyonDmi <xartman90@mail.ru>
2018-02-22 09:02:12 +0700

    master

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+            Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+            Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+            Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+            Console.ReadKey();
+        }
+    }
+}
